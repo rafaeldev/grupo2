7e97a64cb337ae5871280197e8b4191352184392
Rafael Gomes Francisco <rafael.dev@hotmail.com>
2019-12-15 02:52:25 -0200

    show completion skills for job

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,20 @@
 class PagesController < ApplicationController
   def company_home
+    @company = Company.first
+    jobs = @company.jobs
+    students = Student.all
+
+    @target_count = { complete: 0, partial: 0 }
+
+    jobs.each do |job|
+      students.each do |student|
+        intersection = job.skills & student.skills
+
+        return if intersection.size.zero?
+
+        @target_count[intersection.size == job.skills.size ? :complete : :partial] += 1
+      end
+    end
   end
 
   def student_home
