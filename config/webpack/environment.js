a27aaf13725f062930b41537ea6f8d3d1886a696
Rafael Gomes Francisco <rafael.dev@hotmail.com>
2019-12-15 11:48:56 -0200

    install bootstrap dependecies

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,17 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const webpack = require('webpack')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+
 module.exports = environment
